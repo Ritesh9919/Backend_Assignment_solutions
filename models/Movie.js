9b03ae4efb3fb410a723000f1298c6f84901940a
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-11-01 19:11:01 +0530

    created Movie Schema

---
+++ models/Movie.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const movieShema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    yearOfRelease: {
+      type: Number,
+      required: true,
+    },
+    userRatings: {
+      type: Number,
+      required: true,
+    },
+    genre: [String],
+    actors: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Actor",
+    },
+    technicians: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Technician",
+    },
+  },
+  { timestamps: true }
+);
+
+const Movie = mongoose.model("Movie", movieShema);
+module.exports = Movie;
